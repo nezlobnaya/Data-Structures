b6e69e619c32bd8eb09545e06da7c7f3818e83ca
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2020-06-17 22:08:46 -0400

    added a linked list and logic to stack class using ll.still stuck at pop

--- stack/linked_list.py
+++ stack/linked_list.py
@@ -1,12 +1,28 @@
+
 class Node:
-    def __init__(self, value=None, next_node=None):
+    def __init__(self, value=None,prev = None, next_node=None):
         self.value = value
         self.next_node = next_node
+        self.prev = prev
+        
+    
+
 # build a class to manage nodes
 class LinkedList:
-    def __init__(self):
+    def __init__(self, prev = None, node = None, next_node=None):
         self.head = None  # stores a node that is the begining of the list
         self.tail = None  # stores a node that is the end of the list
+        self.prev = prev
+        self.next_node = next_node
+    
+    
+    def delete(self, prev = None):
+        if self.prev:
+            self.prev.next_node = self.next_node
+        if self.next_node:
+            self.next_node.prev = self.prev
+
+
     def add_to_head(self, value):
         # create a node to add
         new_node = Node(value)
@@ -69,6 +85,11 @@ class LinkedList:
                 max_value = current_node.value
             current_node = current_node.next_node
         return max_value
+    
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
 
   
--- stack/stack.py
+++ stack/stack.py
@@ -11,20 +11,29 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
-class Stack:
+from linked_list import LinkedList
+
+
+# Not allowed to use lists
+
+class Stack:# In a stack, a new element is added at one end and an element is removed from that end only. 
     def __init__(self):
         self.size = 0
-        self.storage = []
-
+        # Why is our DLL a good choice to store our elements?
+        self.storage = LinkedList()
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def push(self, value):
-        self.storage.append(value)
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        if len(self.storage) > 0:
-            return self.storage.pop()
-       
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
 
-
+    def len(self):
+        return self.size
